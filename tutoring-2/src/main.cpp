5c2a9624b1e852b156463af217e2bd85abb300cd
Patryk Wertka <spartanpageagain@gmail.com>
2016-02-27 09:20:57 +0100

    main

---
+++ tutoring-2/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+using namespace std;
+
+#include "catch/run-tests.hpp"
+
+int main(int argc, const char *argv[]) {
+    run_tests(argc, argv);
+}
